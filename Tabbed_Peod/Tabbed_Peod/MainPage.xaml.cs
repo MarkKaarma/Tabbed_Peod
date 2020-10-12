a1dd4b8cc01e48904845f55a252fc3004ecbcf16
Mark Kaarma <mark.kaarma@gmail.com>
2020-10-12 20:57:13 +0300

    add new commit

--- Tabbed_Peod/Tabbed_Peod/MainPage.xaml.cs
+++ Tabbed_Peod/Tabbed_Peod/MainPage.xaml.cs
@@ -8,7 +8,7 @@ using Xamarin.Forms;
 
 namespace Tabbed_Peod
 {
-    public partial class MainPage : TabbedPage
+    public partial class MainPage : TabbedPage // Пишем тут вместо Контента,чтобы отображать эти самые таблицы. Идем к Talvel 
     {
         public MainPage()
         {
